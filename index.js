9d2c2f53b987b74d4a4e6ab6aef3d0bbca8680ea
kmturhan <kmturhan@gmail.com>
2019-05-19 05:10:57 +0300

    ready

---
+++ index.js
@@ -0,0 +1,42 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var port = process.env.PORT || 5000;
+var connections = [];
+var users = [];
+app.get('/', function(req,res){
+    res.sendFile(__dirname+'/index.html');
+})
+
+io.on('connection', function(socket){
+    var usersID = socket.id.substr(7,5);
+    connections.push(socket);
+    users.push(usersID);
+    updateUsers();
+    console.log('Kullanici Sayisi: ',connections.length);
+    
+
+    socket.on('disconnect',function(data){
+        users.splice(users.indexOf(usersID),1)
+        updateUsers();
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Kullanici Sayisi: ',connections.length);
+    });
+
+    socket.on('send message', function(msg){
+        var info = [usersID , msg];
+        io.emit('new message', info);
+    });
+ 
+    function updateUsers(){
+        io.emit('get users',users);
+    }
+    
+});
+
+
+
+http.listen(port,function(){
+    console.log('listening on *:5000');
+})
