cf8a83e9c2d994a3756789f196ebe61a68baeb22
kmturhan <kmturhan@gmail.com>
2019-05-20 01:11:09 +0300

    First project code socket.io

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+var socket = io();
+$('#messageForm').submit(function(){
+    
+    socket.emit('send message', $('#text').val());
+    $('#text').val('');
+    return false;
+});
+
+socket.on('new message', function(data){
+    $('.chatBox').prepend(`<div class='rowMessage'><span><b>${data[0]} : </b></span><span> ${data[1]}<span></div>`);
+})
+
+socket.on('get users', function(data){
+    var html = '';
+    data.forEach(function(element) {
+        html += '<li>'+element+'</li>';
+    });
+    
+    $('#userList').html(html);
+})
